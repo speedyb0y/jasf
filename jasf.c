1408d0cb0a2e76074a9c718d89359d28a18fc109
Willian B <speedyb0y@users.noreply.github.com>
2020-04-20 14:36:31 -0300

    Update jasf.c

--- jasf.c
+++ jasf.c
@@ -28,20 +28,20 @@ typedef struct decode_s decode_s;
 #define CHILDS_MASK 0b11U
 #define CHILDS_BITS 2
 
+#define CACHE_INVALID 0xFFFFFU
+
 struct encode_s {
     u64 hash;
     u64 hash1;
     u64 hash2;
-    u32 level;
     u32 index;
     u32 childs[CHILDS_SIZE];
-    u32* ptr;
+    u32* ptr; // TODO: FIXME: transformar em u32
 };
 
 typedef struct encode_context_s_i {
     uint pos;
     uint size;
-    uint headSize;
     uint headMask;
     u64 hash;
     u64 hash1;
@@ -54,7 +54,6 @@ typedef struct encode_context_s_i {
 typedef struct encode_context_s {
     uint pos;
     const uint size;
-    const uint headSize;
     const uint headMask;
     const u64 hash;
     const u64 hash1;
@@ -73,35 +72,66 @@ struct decode_s {
 #define CODE_NEW  0xFFFFFFFFU
 
 #if 1
-static inline uint CACHE_ID(const encode_context_s* const restrict ctx, const uint id) {
-    if (id > ctx->size) // >=
-        abort();
-    return id;
-}
-
-static inline encode_s* CACHE_NODE(const encode_context_s* const restrict ctx, encode_s* const restrict _node) {
-    if (_node < ctx->cache || _node > (ctx->cache + ctx->size))
-        abort();
-    return _node;
-}
-
 // cria um array do tamanho do cache
 //   seta todos como ctx->size
 static inline void VERIFY_CTX(const encode_context_s* const restrict ctx, uint line) {
-
+    printf("OK?\n");
     uint count;
+
+    { u8 array[ctx->size];
+
+        count = ctx->size;
+        while (count--)
+            array[count] = 1;
+
+        count = ctx->size;
+        while (count--)
+            if(ctx->cache[count].ptr)
+                if(*ctx->cache[count].ptr != CACHE_INVALID)
+                    array[*ctx->cache[count].ptr] = 0;
+
+        count = ctx->size;
+        while (count--)
+            if (array[count])
+                printf("AFF %u\n", count);
+    }
 
     count = ctx->size;
 
-    while (--count) {   //  ptr tem q ue estar entre o head ou o .child[] de algum
+    while (count--) {   //  ptr tem q ue estar entre o head ou o .child[] de algum
+
         // All cache[indexes[x]].index -> x
         if (ctx->cache[ctx->indexes[count]].index != count)
             abort();
+
         // All heads are 0 or its target points to it in it s ptr
         if (ctx->cache[count].ptr != NULL && *ctx->cache[count].ptr != count) {
             printf("LINE %u ID %u?\n", line, count);
             abort();
         }
+
+        const encode_s* const this = &ctx->cache[count];
+
+        // Seu ptr não pode apontar para um próprio slot de child
+        if (this->ptr == &this->childs[0] ||
+            this->ptr == &this->childs[1] ||
+            this->ptr == &this->childs[2] ||
+            this->ptr == &this->childs[3]
+            ) abort();
+
+        // O valor no slot de seu child não pode ser si mesmo
+        if (this == (ctx->cache + this->childs[0]) ||
+            this == (ctx->cache + this->childs[1]) ||
+            this == (ctx->cache + this->childs[2]) ||
+            this == (ctx->cache + this->childs[3])
+            ) abort();
+
+        //
+        if ((this->childs[0] > ctx->size && this->childs[0] != CACHE_INVALID) ||
+            (this->childs[1] > ctx->size && this->childs[1] != CACHE_INVALID) ||
+            (this->childs[2] > ctx->size && this->childs[2] != CACHE_INVALID) ||
+            (this->childs[3] > ctx->size && this->childs[3] != CACHE_INVALID)
+            ) abort();
     }
 
     // All items in cache
@@ -110,55 +140,36 @@ static inline void VERIFY_CTX(const encode_context_s* const restrict ctx, uint l
         // it's ptr is not itself
         // it's childs are not itself
         // all ids are 0 <= id < size
-
-    //count = ctx->size;
-    ////while (--count)
-        //printf("%u ", ctx->cache[count].level);
-    //printf("\n");
+    printf("OK.\n");
 }
 
 #define VERIFY_CTX(ctx) VERIFY_CTX((ctx), __LINE__)
-#define CACHE_ID(_id) CACHE_ID(ctx, (_id))
-#define CACHE_NODE(_node) CACHE_NODE(ctx, (_node))
-#else
-#define CACHE_ID(id) (id)
 #endif
 
-#define FAZ(var, id) (CACHE_NODE((var) = ctx->cache + CACHE_ID(id)) != (encode_s*)ctx->indexes)
+static inline encode_s* leaf_node(encode_context_s* const restrict ctx, encode_s* const restrict node) {
 
-#define FOLLOW(_node, level, ptr) ({ \
-    (ptr) = &(CACHE_NODE(_node))->childs[(hash >> (level * (level < (64 - CHILDS_BITS)))) & CHILDS_MASK]; \
-    level += CHILDS_BITS; \
-    })
+    //printf("LEAF %p\n", node);
 
-#define SET_LEVEL_LINK(_node, _level, _ptr) ({ \
-    (CACHE_NODE(_node))->level = (_level); \
-    (_node)->ptr = (_ptr); \
-    *(_ptr) = CACHE_ID((_node) - ctx->cache); \
-    })
+    if (!((ctx->cache + node->childs[0]) != node &&
+          (ctx->cache + node->childs[1]) != node &&
+          (ctx->cache + node->childs[2]) != node &&
+          (ctx->cache + node->childs[3]) != node
+        )) abort();
 
-#define VERIFY_NODE(_node) ({ \
-    if ((_node)->childs[0] == ((_node) - ctx->cache) || \
-        (_node)->childs[1] == ((_node) - ctx->cache) || \
-        (_node)->childs[2] == ((_node) - ctx->cache) || \
-        (_node)->childs[3] == ((_node) - ctx->cache) || \
-        (_node)->childs[0] > ctx->size || \
-        (_node)->childs[1] > ctx->size || \
-        (_node)->childs[2] > ctx->size || \
-        (_node)->childs[3] > ctx->size \
-        ) { \
-        printf("LINE %d\n", __LINE__); \
-        abort(); \
-    } \
-    if (ctx->indexes[(_node)->index] != ((_node) - ctx->cache)) { \
-        printf("LINE %d   (ctx->indexes[(_node)->index] != ((_node) - ctx->cache)) \n", __LINE__); \
-        abort(); \
-    } \
-    if (!((_node)->ptr == NULL || *(_node)->ptr <= ctx->size)) { \
-        printf("LINE %d (!((_node)->ptr == NULL || *(_node)->ptr <= ctx->size)) \n", __LINE__); \
-        abort(); \
-    } \
-})
+    if (node->childs[0] != CACHE_INVALID) return leaf_node(ctx, ctx->cache + node->childs[0]);
+    if (node->childs[1] != CACHE_INVALID) return leaf_node(ctx, ctx->cache + node->childs[1]);
+    if (node->childs[2] != CACHE_INVALID) return leaf_node(ctx, ctx->cache + node->childs[2]);
+    if (node->childs[3] != CACHE_INVALID) return leaf_node(ctx, ctx->cache + node->childs[3]);
+
+    //uint slot = CHILDS_SIZE;
+
+    //do { const uint id = node->childs[slot];
+        //if (id != CACHE_INVALID)
+            //return leaf_node(ctx, ctx->cache + id);
+    //} while (slot--);
+
+    return node;
+}
 
 // a soma d etodos os indexes
 // a soma de todos os estrutura->codigo
@@ -188,87 +199,125 @@ static uint lookup(encode_context_s* const restrict ctx, const void* restrict st
         len -= sizeof(u8);
     }
 
-    uint level = 0;
-    u32* ptr = &ctx->heads[hash1 &  ctx->headMask];
-    encode_s* this;
+    encode_s* const cache = ctx->cache;
 
-    while FAZ(this, *ptr) {
-        VERIFY_NODE(this);
+    uint level = 0;
+
+    u32* ptr = ctx->heads + (hash1 & ctx->headMask);
+
+    loop {
+        const uint id = *ptr;
+
+        if (id == CACHE_INVALID) {
+            /* Não encontrou */
+
+            encode_s* const this = cache + ctx->indexes[ctx->pos++ % ctx->size]; // Sobrescreve o mais antigo
+            encode_s* const leaf = leaf_node(ctx, this); // Encontra um leaf dele para substituir ele
+
+            if (leaf->childs[0] != CACHE_INVALID) abort();
+            if (leaf->childs[1] != CACHE_INVALID) abort();
+            if (leaf->childs[2] != CACHE_INVALID) abort();
+            if (leaf->childs[3] != CACHE_INVALID) abort();
+
+            if (this->ptr == NULL) { // Está usando um que ainda não foi usado
+
+               if (this->childs[0] != CACHE_INVALID) abort();
+               if (this->childs[1] != CACHE_INVALID) abort();
+               if (this->childs[2] != CACHE_INVALID) abort();
+               if (this->childs[3] != CACHE_INVALID) abort();
+
+                this->ptr = ptr;
+               *this->ptr = this - cache;
+            } elif ((void*)this <= (void*)ptr && (void*)ptr < ((void*)this + sizeof(encode_s))) {
+                // Está usando o que seria o parent
+                // this->ptr mantém
+                // ptr aponta para o slot vazio que seguiu
+                // Os demais slots podem conter outras coisas
+               if (&this->childs[0] != ptr &&
+                   &this->childs[1] != ptr &&
+                   &this->childs[2] != ptr &&
+                   &this->childs[3] != ptr) { printf("%d\n", __LINE__); abort(); }
+
+                *ptr = CACHE_INVALID;
+            } elif (this == leaf) { // Escolheu um que não tem childs; não precisa mover nada
+
+               if (this->childs[0] != CACHE_INVALID) abort();
+               if (this->childs[1] != CACHE_INVALID) abort();
+               if (this->childs[2] != CACHE_INVALID) abort();
+               if (this->childs[3] != CACHE_INVALID) abort();
+
+               *this->ptr = CACHE_INVALID;
+                this->ptr = ptr;
+               *this->ptr = this - cache;
+            } else {
+                // Escolheu um que tem childs, e selecionou um leaf abaixo dele
+                // Este leaf pode ser um child direto do this, desmarca essa relação
+               *leaf->ptr = CACHE_INVALID;
+                leaf->ptr = this->ptr;
+               *leaf->ptr = leaf - cache; // this->ptr sendo sobrescrito aqui
+                // Passa todos os demais para o leaf
+                // Os que realmente existirem, vai neles e muda o ptr deles
+                if ((leaf->childs[0] = this->childs[0]) != CACHE_INVALID) (cache + leaf->childs[0])->ptr = &leaf->childs[0];
+                if ((leaf->childs[1] = this->childs[1]) != CACHE_INVALID) (cache + leaf->childs[1])->ptr = &leaf->childs[1];
+                if ((leaf->childs[2] = this->childs[2]) != CACHE_INVALID) (cache + leaf->childs[2])->ptr = &leaf->childs[2];
+                if ((leaf->childs[3] = this->childs[3]) != CACHE_INVALID) (cache + leaf->childs[3])->ptr = &leaf->childs[3];
+                // Agora sim usa o this
+                this->ptr = ptr;
+               *this->ptr = this - cache;
+                this->childs[0] = CACHE_INVALID;
+                this->childs[1] = CACHE_INVALID;
+                this->childs[2] = CACHE_INVALID;
+                this->childs[3] = CACHE_INVALID;
+            }
+
+            this->hash  = hash;
+            this->hash1 = hash1;
+            this->hash2 = hash2;
+
+            if (*this->ptr != (this - cache)) abort();
+
+            if (this->ptr == &this->childs[0]) abort();
+            if (this->ptr == &this->childs[1]) abort();
+            if (this->ptr == &this->childs[2]) abort();
+            if (this->ptr == &this->childs[3]) abort();
+
+            if (*this->ptr == this->childs[0]) abort();
+            if (*this->ptr == this->childs[1]) abort();
+            if (*this->ptr == this->childs[2]) abort();
+            if (*this->ptr == this->childs[3]) abort();
+
+            return CODE_NEW;
+        }
+
+        encode_s* const this = cache + id;
+
         if (this->hash  == hash  &&
             this->hash1 == hash1 &&
             this->hash2 == hash2) {
+            /* Encontrou */
+
             int code = ctx->pos - this->index;
+
             if (code == 0) {
                 return CODE_SAME;
             }
+
             if (code == 1) {
                 return code;
             }
+
             // TODO: FIXME: mover ele para frente
             // e se não foi o último , coloca roúltimo sobre ele
             return code;
-        } FOLLOW(this, level, ptr); // TODO: FIXME: vai dar certo isso?
-    }
-
-    // Sobrescreve o mais antigo
-    this = ctx->cache + ctx->indexes[ctx->pos++ % ctx->size];
-
-    if (((void*)this > (void*)ptr) || (((void*)this + sizeof(encode_s)) <= (void*)ptr)) {
-        // O escolhido não foi o último do path que seguiu
-        if (this->ptr) {
-            // Estamos em uma hash tree, podendo ser outra
-            // Para o caso deste ser o último
-            *this->ptr = ctx->size;
-            uint slot = CHILDS_SIZE;
-            // Acha o primeiro child
-            while (slot--) { encode_s* child0;
-                if FAZ(child0, this->childs[slot]) {
-                    // O passa para o parent
-                    SET_LEVEL_LINK(child0, this->level, this->ptr);
-                    // Os demais viram filhos do primeiro, a partir do mesmo slot
-                    while (slot--) { encode_s* child;
-                        if FAZ(child, this->childs[slot]) { encode_s* this;
-                            u32* ptr = &child0->childs[slot];
-                            const u64 hash = child->hash;
-                            uint level = child->level;
-                            // Vai até o fim
-                            while FAZ(this, *ptr)
-                                FOLLOW(this, level, ptr);
-                            SET_LEVEL_LINK(child, level, ptr);
-                        }
-                    } break;
-                }
-            }
         }
 
-        VERIFY_NODE(this);
-
-        *(this->ptr = ptr) = this - ctx->cache;
+        // CHILDS_BITS tem que ser divisor de 64
+        //_ptr = &(_node)->childs[(hash >> (_level < (64 - CHILDS_BITS)))) & CHILDS_MASK];
+        ptr = &this->childs[(hash >> level) & CHILDS_MASK];
+        // TODO: FIXME: vai dar certo isso?
+        if ((level += CHILDS_BITS) > 60)
+            abort();
     }
-#if 1
-    else {
-        // this->ptr == ptr
-        // ptr = este
-        // este->index
-    }
-#endif
-
-    VERIFY_NODE(this);
-
-    this->hash  = hash;
-    this->hash1 = hash1;
-    this->hash2 = hash2;
-    this->level = level;
-    this->childs[0] = ctx->size;
-    this->childs[1] = ctx->size;
-    this->childs[2] = ctx->size;
-    this->childs[3] = ctx->size;
-
-    VERIFY_NODE(this);
-
-    VERIFY_CTX(ctx);
-
-    return CODE_NEW;
 }
 
 static inline u64 rdtscp(void) {
@@ -284,7 +333,13 @@ static encode_context_s* encoder_new (const uint size, const uint headSize, u64 
         return NULL;
 
     // TODO: FIXME: ver se esse valor está correto aqui
-    if (size >= 0b11111111111111111111)
+    if (size >= 0b11111111111111111111U)
+        return NULL;
+
+    if (size >= CACHE_INVALID)
+        return NULL;
+
+    if (headSize > size)
         return NULL;
 
     const uint headMask = headSize - 1;
@@ -305,7 +360,7 @@ static encode_context_s* encoder_new (const uint size, const uint headSize, u64 
         case 0b1111111111111U: // 8192
         case 0b11111111111111U:
         case 0b111111111111111U: // 32767
-        case 0b1111111111111111U:
+        case 0b1111111111111111U: // 65535
             break;
         default:
             return NULL;
@@ -313,22 +368,23 @@ static encode_context_s* encoder_new (const uint size, const uint headSize, u64 
 
     hash  += rdtscp();
     hash1 += hash;
-    hash2 += ~hash;
+    hash2 += hash1 >> 32;
 
-    encode_context_s_i* const ctx = malloc(sizeof(encode_context_s) + size*sizeof(encode_s) + size*sizeof(u32) + headSize*sizeof(u32*));
+    encode_context_s_i* const ctx = malloc(sizeof(encode_context_s) + size*sizeof(encode_s) + size*sizeof(u32) + headSize*sizeof(u32));
 
     if (ctx == NULL)
         return NULL;
 
     ctx->pos = 0;
     ctx->size = size;
+    ctx->hash  = hash;
+    ctx->hash1 = hash1;
+    ctx->hash2 = hash2;
     ctx->indexes = (void*)(ctx->cache + size);
     ctx->heads = (void*)(ctx->indexes + size);
     ctx->headMask = headMask;
-    ctx->hash = hash;
-    ctx->hash1 = hash1;
-    ctx->hash2 = hash2;
 
+    // Initialize ctx->cache and ctx->indexes
     uint count = 0;
 
     do {
@@ -337,12 +393,11 @@ static encode_context_s* encoder_new (const uint size, const uint headSize, u64 
         this->hash  = 0;
         this->hash1 = 0;
         this->hash2 = 0;
-        this->level = 0;
         this->index = count;
-        this->childs[0] = size;
-        this->childs[1] = size;
-        this->childs[2] = size;
-        this->childs[3] = size;
+        this->childs[0] = CACHE_INVALID;
+        this->childs[1] = CACHE_INVALID;
+        this->childs[2] = CACHE_INVALID;
+        this->childs[3] = CACHE_INVALID;
         this->ptr = NULL;
 
         ctx->indexes[count] = count;
@@ -352,7 +407,7 @@ static encode_context_s* encoder_new (const uint size, const uint headSize, u64 
     count = 0;
 
     do {
-        ctx->heads[count] = size;
+        ctx->heads[count] = CACHE_INVALID;
     } while (++count != headSize);
 
     return (encode_context_s*)ctx;
@@ -360,9 +415,39 @@ static encode_context_s* encoder_new (const uint size, const uint headSize, u64 
 
 static void fill(encode_context_s* const ctx, uintll count) {
 
-    static u64 random1 = 0x77545b4578540574ULL; //rdtscp();
-
-    static u64 random[32] = {
+    static u64 buff[64] = {
+        0xa405f056054046e4ULL,
+        0x01d345f7893BaDEFULL,
+        0x480fdbff42ff0134ULL,
+        0x4aa4f544f0fa6405ULL,
+        0x4f0436a564015456ULL,
+        0x5f42a5a45da52d78ULL,
+        0x4a6faf3875405744ULL,
+        0x540f845645246544ULL,
+        0x14545b4578540574ULL,
+        0x456074745424a407ULL,
+        0x445604fa403af044ULL,
+        0x456f4587a3f54060ULL,
+        0xe0f40460735dedf5ULL,
+        0x0847f974623a4650ULL,
+        0x04f05d5ff4065f47ULL,
+        0x0054074050507007ULL,
+        0xa405f056054046e4ULL,
+        0x01d345f7893BaDEFULL,
+        0x480fdbff42ff0134ULL,
+        0x4aa4f544f0fa6405ULL,
+        0x4f0436a564015456ULL,
+        0x5f42a5a45da52d78ULL,
+        0x4a6faf3875405744ULL,
+        0x540f845645246544ULL,
+        0x14545b4578540574ULL,
+        0x456074745424a407ULL,
+        0x445604fa403af044ULL,
+        0x456f4587a3f54060ULL,
+        0xe0f40460735dedf5ULL,
+        0x0847f974623a4650ULL,
+        0x04f05d5ff4065f47ULL,
+        0x0054074050507007ULL,
         0xa405f056054046e4ULL,
         0x01d345f7893BaDEFULL,
         0x480fdbff42ff0134ULL,
@@ -397,19 +482,22 @@ static void fill(encode_context_s* const ctx, uintll count) {
         0x0054074050507007ULL,
         };
 
+    u64 random1 = rdtscp();
+
     while (count--) {
-        random[0] += random1;
-        random[1] += random1;
-        random[2] += random1;
-        random[3] += random1;
-        random[4] += random1;
-        random[5] += random1;
-        random[6] += random1;
-        random[7] += random1;
-        random[8] += random1;
+        buff[0] += random1++;
+        buff[1] += random1++;
+        buff[2] += random1++;
+        buff[3] += random1++;
+        buff[4] += random1++;
+        buff[5] += random1++;
+        buff[6] += random1++;
+        buff[7] += random1++;
+        buff[8] += random1++;
         random1 += random1 << (count   & 0b11111U);
         random1 += count   << (random1 & 0b11111U);
-        lookup(ctx, (void*)random, 1 + (random1 & 0b111111U));
+        random1++;
+        lookup(ctx, (void*)buff, 1 + (random1 & 0b111111U));
     }
 }
 
@@ -419,11 +507,18 @@ int main (void) {
 
     encode_context_s* const ctx = encoder_new(CACHE_SIZE, 64, 0, 0, 0);
 
-    VERIFY_CTX(ctx); fill(ctx, 1000*CACHE_SIZE);
-    VERIFY_CTX(ctx); fill(ctx, 1000*CACHE_SIZE);
-    VERIFY_CTX(ctx); fill(ctx, 3*CACHE_SIZE);
-    VERIFY_CTX(ctx); fill(ctx, 2*CACHE_SIZE);
-    VERIFY_CTX(ctx); fill(ctx, 2*CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, 100*CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
+    VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
     VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
     VERIFY_CTX(ctx); fill(ctx, CACHE_SIZE);
     VERIFY_CTX(ctx);
